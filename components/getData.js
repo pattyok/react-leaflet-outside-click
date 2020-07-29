3f20fa5b15ea58efb0bb0eba213dcc2a3a41698b
pattyok <patty.ohara@gmail.com>
2020-07-29 14:26:06 -0700

    2 files modified, 1 deleted

--- components/getData.js
+++ components/getData.js
@@ -1,19 +1,44 @@
 
 let markerData =[];
 
-const items = document.querySelectorAll("#mapped-posts-data li");
-items.forEach(item => {
-    const name = item.getElementsByClassName('wp-block-mapped-posts-archive__text-link')[0];
+const locations = [
+  {
+    name: "Oslo",
+    lat: 59.923043,
+    lng: 10.752839
+  },
+  {
+    name: "Stockholm",
+    lat: 59.339025,
+    lng: 18.065818
+  },
+  {
+    name: "Copenhagen",
+    lat: 55.675507,
+    lng: 12.574227
+  },
+  {
+    name: "Berlin",
+    lat: 52.521248,
+    lng: 13.399038
+  },
+  {
+    name: "Paris",
+    lat: 48.856127,
+    lng: 2.346525
+  }
+];
+
+locations.forEach((item, index) => {
     const data = {
-        position : [item.getAttribute("data-lat"), item.getAttribute("data-lng")],
-        content : item.innerHTML,
-        id : item.getAttribute("data-id"),
-        name: name.textContent
+        position : [item.lat, item.lng],
+        content : item.name,
+        id : index,
+        name: item.name,
     }
     markerData.push(data);
 
 });
-
 
 
 export default markerData;
--- index.js
+++ index.js
@@ -3,33 +3,7 @@ import { render } from 'react-dom';
 import Map from './components/Map';
 import './style.css';
 
-const locations = [
-  {
-    name: "Oslo",
-    lat: 59.923043,
-    lng: 10.752839
-  },
-  {
-    name: "Stockholm",
-    lat: 59.339025,
-    lng: 18.065818
-  },
-  {
-    name: "Copenhagen",
-    lat: 55.675507,
-    lng: 12.574227
-  },
-  {
-    name: "Berlin",
-    lat: 52.521248,
-    lng: 13.399038
-  },
-  {
-    name: "Paris",
-    lat: 48.856127,
-    lng: 2.346525
-  }
-];
+
 
 
 function App(){
