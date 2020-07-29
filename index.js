3054d02e556f05383e2038729ab2708280cec0bf
pattyok <patty.ohara@gmail.com>
2020-07-29 14:28:26 -0700

    1 file modified

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Map from './components/Map';
+import markerData from "./components/getData";
 import './style.css';
 
 
-
+const bounds = L.latLngBounds()
+  markerData.forEach((data) => {
+    bounds.extend(data.position)
+  })
 
 function App(){
-  return (<Map zoom={4} center={{lat:53.80,lng: 12.63}} locations={locations} />);
+  return (<Map zoom={4} bounds={bounds} maxZoom={10} locations={markerData} />);
 }
 
 render(<App/>, document.getElementById('root'));
