08379167f1f9533088fe4b5aba2a1265b5be45b9
pattyok <patty.ohara@gmail.com>
2020-07-30 08:28:36 -0700

    1 file modified

--- components/Pins.js
+++ components/Pins.js
@@ -4,78 +4,13 @@ import MarkerClusterGroup from 'react-leaflet-markercluster';
 import 'react-leaflet-markercluster/dist/styles.min.css';
 import Pin from './Pin.js';
 
-const hexToRgb = (hex, opacity) => {
-    if (!hex.startsWith('#')) return hex;
-    const hashless = hex.slice(1);
-    const num = parseInt(
-      hashless.length === 3
-        ? hashless.split('').map(c => c.repeat(2)).join('')
-        : hashless,
-      16,
-    );
-    const red = num >> 16;
-    const green = (num >> 8) & 255;
-    const blue = num & 255;
-
-    if (opacity) {
-      return `rgba(${red}, ${green}, ${blue}, ${opacity})`;
-    }
-    return `rgb(${red}, ${green}, ${blue})`;
-  };
 
 const Pins = (props) => {
     //const { pins } = props;
     const { data, selectedIndex, onItemClick } = props;
     const groupRef = useRef(null);
 
-  //   useEffect(() => {
-  //   const group = groupRef.current.leafletElement; //get leaflet.markercluster instance  
-  //   group.zoomToShowLayer()
-  // }, []);
-
-    const createClusterCustomIcon = (cluster) => {
-        const count = cluster.getChildCount();
-        let size = 'LargeXL';
-
-        if (count < 10) {
-          size = 'Small';
-        }
-        else if (count >= 10 && count < 100) {
-          size = 'Medium';
-        }
-        else if (count >= 100 && count < 500) {
-          size = 'Large';
-        }
-        const options = {
-          cluster: `markerCluster${size}`,
-          circle1: `markerCluster${size}DivOne`,
-          circle2: `markerCluster${size}DivTwo`,
-          circle3: `markerCluster${size}DivThree`,
-          circle4: `markerCluster${size}DivFour`,
-          label: `markerCluster${size}Label`,
-        };
-
-        const clusterColor = hexToRgb('#fc4b51');
-        const circleStyle1 = `background-color: ${clusterColor.slice(0, -1)}, 0.05)`;
-        const circleStyle2 = `background-color: ${clusterColor.slice(0, -1)}, 0.15)`;
-        const circleStyle3 = `background-color: ${clusterColor.slice(0, -1)}, 0.25)`;
-        const circleStyle4 = `background-color: ${clusterColor.slice(0, -1)}, 0.65)`;
-
-
-        return L.divIcon({
-          html:
-            `<div style="${circleStyle1}" class="${options.circle1}">
-                        <div style="${circleStyle2}" class="${options.circle2}">
-                            <div style="${circleStyle3}" class="${options.circle3}">
-                                <div style="${circleStyle4}" class="${options.circle4}">
-                                    <span class="${options.label}">${count}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>`,
-          className: `${options.cluster}`,
-        });
-      };
+  
       const Markers = data.map((item) => (
         <Pin
         key={item.id}
@@ -91,7 +26,6 @@ const Pins = (props) => {
     return (
       <MarkerClusterGroup
         ref={groupRef}
-        iconCreateFunction={createClusterCustomIcon}
         showCoverageOnHover={false}
         spiderfyOnMaxZoom={true}
         spiderLegPolylineOptions={{
